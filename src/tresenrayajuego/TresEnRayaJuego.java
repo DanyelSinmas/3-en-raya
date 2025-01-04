5ae67c8619a7f3d1d709300fcd89e04da7f48d1e
DanyelSinmas <danielmartinpostigo03@gmail.com>
2025-01-04 20:21:21 +0100

    CPU terminada

--- src/tresenrayajuego/TresEnRayaJuego.java
+++ src/tresenrayajuego/TresEnRayaJuego.java
@@ -13,6 +13,7 @@ public class TresEnRayaJuego {
         //declaración de variables
         boolean play = true; //booleano que comprueba si se quiere volver a jugar tras una ronda o al iniciar el programa
         int fila = 0; //numero que determina en que fila de la matriz imprimes el carácter
+        int filaComprobar = 0, columnaComprobar = 0; //cuando se sabe que hay que hace una jugada en la fila o columna, se guarda dicha fila/columna en una variable para más adelante
         int columna = 0; //numero que determina en que columna de la matriz imprimes el carácter
         char tablero[][] = { //matriz 3x3 que representa el tablero de juego
             {' ', ' ', ' '},
@@ -21,9 +22,11 @@ public class TresEnRayaJuego {
         };
         int numJugador = 0; //numero que define si es el turno de X o de O
         int opcionIni = 0; //la opcion de inicio del user
+        int contadorFilas = 0, contadorColumnas = 0;//contadores para determinar si se hace una jugada específica o no
+        boolean jugadaFilas = false, jugadaColumnas = false;//flags para ayudarme
         Scanner reader = new Scanner(System.in);
 
-        //código del juego:
+        //CÓDIGO DEL JUEGO:
         System.out.println("Bienvenido al juego del 3 en raya");
         System.out.println("1 Para jugar\n2 Para ver las reglas y empezar la partida\n3 Para salir");
         do { //en este do while se controlan que las opciones del menú no den excepciones
@@ -75,8 +78,60 @@ public class TresEnRayaJuego {
                         reader.next();
                     }
                 } else {
-                    fila = (int) (Math.random() * 3) + 1;
-                    columna = (int) (Math.random() * 3) + 1;
+                    //turno de la CPU
+                    //comprobacion de las filas
+                    for (int i = 0; i < 3 && jugadaFilas == false; i++) {
+                        for (int j = 0; j < 3 && jugadaFilas == false; j++) {
+                            if (tablero[i][j] != ' ') {
+                                contadorFilas += 5;
+                                if (contadorFilas == 10) {
+                                    jugadaFilas = true;
+                                    filaComprobar = i;
+                                    fila = i + 1;
+                                }
+                            } else if (tablero[i][j] == ' ') {
+                                contadorFilas = 0;
+                            }
+                        }
+                    }
+                    //comprobacion de las columnas
+                    for (int i = 0; i < 3 && jugadaColumnas == false; i++) {//sigue siendo lo mismo que para las filas pero intercambiando j e i para recorrer columnas en vez de filas
+                        for (int j = 0; j < 3 && jugadaColumnas == false; j++) {
+                            if (tablero[j][i] != ' ') {
+                                contadorColumnas += 5;
+                                if (contadorColumnas == 10) {
+                                    jugadaColumnas = true;
+                                    columnaComprobar = i;
+                                    columna = i + 1;
+                                }
+                            } else if (tablero[j][i] == ' ') {
+                                contadorColumnas = 0;
+                            }
+                        }
+                    }
+                    if (jugadaFilas == true) {//se determina la columna
+                        for (int k = 0; k < 3 && jugadaFilas == true; k++) {
+                            if (tablero[filaComprobar][k] == ' ') {
+                                columna = k + 1;
+                            } else if (tablero[filaComprobar][0] != ' ' && tablero[filaComprobar][1] != ' ' && tablero[filaComprobar][2] != ' ') {
+                                jugadaFilas = false;
+                            }
+                        }
+                    }
+                    if (jugadaColumnas == true) {//se determina la fila
+                        for (int l = 0; l < 3 && jugadaColumnas == true; l++) {
+                            if (tablero[l][columnaComprobar] == ' ') {
+                                fila = l + 1;
+                            } else if (tablero[0][columnaComprobar] != ' ' && tablero[1][columnaComprobar] != ' ' && tablero[2][columnaComprobar] != ' ') {
+                                jugadaColumnas = false;
+                            }
+                        }
+                    }
+                    if (jugadaColumnas == false && jugadaFilas == false) {
+                        fila = (int) (Math.random() * 3) + 1;
+                        columna = (int) (Math.random() * 3) + 1;
+                    }
+
                 }
                 if (fila >= 1 && fila <= 3 && columna >= 1 && columna <= 3) { //este if no permite un valor que provoce excepciones junto al try-catch
                     if (tablero[fila - 1][columna - 1] == ' ') { //este if comprueba que la celda seleccionada esté ya ocupada
@@ -113,6 +168,8 @@ public class TresEnRayaJuego {
             }
             System.out.println("");
             numJugador++; //el número de jugador suma, dando turno al siguiente.
+            contadorFilas = 0; //el contador de filas se reinicia
+            contadorColumnas = 0; //el contador de columnas se reinicia
         }
 
         System.out.println("Hasta la próxima!");
